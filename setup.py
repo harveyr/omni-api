391193ec2087fd0f7c1881001b6bc21b035b6a44
Harvey Rogers <harveyr@gmail.com>
2014-07-19 12:21:01 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='omni-api',
+    version='0.1',
+    description='API stuffs',
+    author='Harvey Rogers',
+    author_email='harveyr@gmail.com',
+    packages=find_packages(),
+)
